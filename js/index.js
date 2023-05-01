64f8827eb6c63a3f1c0e1a71133d62db0b2f112c
EUNSOLY <dmsthf9596@gmail.com>
2023-05-01 12:59:25 +0900

    23.05.01 카카오뱅크 클론코딩 5일차

--- js/index.js
+++ js/index.js
@@ -8,3 +8,29 @@ hamBtn.addEventListener("click", (e) => {
   gnb.classList.toggle("on");
   hd.classList.toggle("on");
 });
+
+function scrollEvent() {
+  const ImgCon = document.querySelector(".fixedContent.section2");
+  let scroll = window.innerHeight + window.scrollY;
+  const Img = document.querySelector(
+    ".fixedContent.section2 .contentRight img"
+  );
+  let ImgHeight = ImgCon.offsetTop;
+  if (ImgHeight < scroll) {
+    Img.classList.add("on");
+  }
+}
+function scrollEventBootom() {
+  const ImgCon = document.querySelector(".contentWrap.section5");
+  const Img = document.querySelector(".contentWrap.section5 .contentLeft img");
+  let scroll = window.innerHeight + window.scrollY;
+  let ImgHeight = ImgCon.offsetTop;
+
+  if (ImgHeight < scroll) {
+    Img.classList.add("on");
+  }
+}
+window.addEventListener("scroll", function () {
+  scrollEvent();
+  scrollEventBootom();
+});
