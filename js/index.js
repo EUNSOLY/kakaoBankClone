3979bde859ef1178292f9704162a566666959c89
EUNSOLY <dmsthf9596@gmail.com>
2023-05-03 17:58:47 +0900

    23.05.03 카카오뱅크 클론코딩 7일차

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ function scrollEventBootom() {
 }
 function scrollEventStar() {
   const section = document.querySelector(".section6");
-  let scroll = window.innerHeight + window.scrollY - 400;
+  let scroll = window.innerHeight + window.scrollY;
   let sectionHeight = section.offsetTop;
   if (sectionHeight < scroll) {
     section.classList.add("night");
@@ -45,3 +45,37 @@ window.addEventListener("scroll", function () {
   scrollEventBootom();
   scrollEventStar();
 });
+
+// 슬라이드 구현
+
+// DOM 변수에 담기
+const slideCon = document.querySelector(".slideList");
+const slide = document.querySelectorAll(".slideCard");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+
+// 초기값
+let currentIndex = 0;
+let slideCount = slide.length;
+let slideWidth = slide[0].offsetWidth;
+
+// 이전 버튼 클릭 시
+
+// slide.forEach((item, i) => {
+//   item.style.cssText = `margin-right:-20px;`;
+// });
+
+const prevBtnFun = () => {
+  slideCon.style.cssText = `margin-left: -${slideWidth}px; transition:2s`;
+  setTimeout(() => {
+    slideCon.appendChild(slideCon.firstElementChild);
+    slideCon.style.cssText = `margin-left:0px;`;
+  }, 5);
+  // slide.forEach((item, i) => {
+  //   item.style.cssText = `transition: 1s;`;
+  // });
+};
+const nextBtnFun = () => {};
+
+prevBtn.addEventListener("click", prevBtnFun);
+nextBtn.addEventListener("click", nextBtnFun);
